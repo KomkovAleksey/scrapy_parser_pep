eff2b57f5fd96d9b289589d49f6dae91c75b1d47
Алексей Комков <ale45883412@yandex.ru>
2024-01-16 12:59:35 +0700

    first commit

---
+++ pep_parse/spiders/pep.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class PepSpider(scrapy.Spider):
+    name = 'pep'
+    allowed_domains = ['peps.python.org']
+    start_urls = ['http://peps.python.org/']
+
+    def parse(self, response):
+        pass
