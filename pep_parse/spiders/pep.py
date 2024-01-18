97ed69af4caa662c52dec005c3530fbf4ccd15c8
Алексей Комков <ale45883412@yandex.ru>
2024-01-19 03:57:44 +0700

    Создал паука PepSpider

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,10 +1,26 @@
 import scrapy
 
+from ..items import PepParseItem
+from ..constants import START_URLS, PEP_SPIDER_NAME, ALLOWED_DOMAINS
+
 
 class PepSpider(scrapy.Spider):
-    name = 'pep'
-    allowed_domains = ['peps.python.org']
-    start_urls = ['http://peps.python.org/']
+    """Паук для парсинга данных из документации PEP."""
+
+    name = PEP_SPIDER_NAME
+    allowed_domains = ALLOWED_DOMAINS
+    start_urls = START_URLS
 
     def parse(self, response):
-        pass
+        """Парсинг ссылок документаций PEP."""
+        for pep_link in response.css('tbody tr a[href^="pep-"]'):
+            yield response.follow(pep_link, callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        """Парсинг данных из ссылок документаций PEP."""
+        data = {
+            'number': response.css('h1.page-title::text').get().split()[1],
+            'name': response.css('h1.page-title::text').get().split(' – ')[1],
+            'status': response.css('abbr::text').get(),
+        }
+        yield PepParseItem(data)
