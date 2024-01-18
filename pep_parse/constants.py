e677575156ec96d4d96bdad919ccdc9226fe8832
Алексей Комков <ale45883412@yandex.ru>
2024-01-19 03:59:56 +0700

    Добавил константы

---
+++ pep_parse/constants.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent.parent
+RESULTS_DIR = 'results'
+
+DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
+
+PEP_DOMAIN = 'peps.python.org'
+ALLOWED_DOMAINS = (PEP_DOMAIN,)
+START_URLS = [f'https://{PEP_DOMAIN}/',]
+PEP_SPIDER_NAME = 'pep'
